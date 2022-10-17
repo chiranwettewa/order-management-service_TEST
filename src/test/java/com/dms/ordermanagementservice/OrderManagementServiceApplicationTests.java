fee4c0a1d84c8edf869e410dd9674491fa612540
chiranwettewa <chiranwettewa@gmail.com>
2022-10-17 19:03:40 +0530

    initial

---
+++ src/test/java/com/dms/ordermanagementservice/OrderManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.ordermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
