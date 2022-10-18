75ae2147420411b0acc9bafd44c703ec15c4ee7f
chiranwettewa <chiranwettewa@gmail.com>
2022-10-18 18:07:52 +0530

    final push

--- src/main/java/com/dms/ordermanagementservice/repository/OrderRepository.java
+++ src/main/java/com/dms/ordermanagementservice/repository/OrderRepository.java
@@ -3,6 +3,4 @@ package com.dms.ordermanagementservice.repository;
 import com.dms.ordermanagementservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order,Long> {
-
-}
+public interface OrderRepository extends JpaRepository<Order,Long> {}
